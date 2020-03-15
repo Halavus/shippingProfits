80d01a41674ad75d483dd1f27d0d5e0d2881cb4a
halavus <halavus@gmail.com>
2020-03-15 12:07:28 +0100

    SAVEPOINT

--- tester.py
+++
@@ -1,8 +0,0 @@
-import json, sys
-with open(sys.argv[1]) as f:
-    data = json.load(f)
-
-def t(product, cx, data):
-    output = data["comex"]["broker"]["brokers"][product+"."+cx]
-    return output
-
